7bcfb538cc9ffcdb04fa8ee614a440b7db6889d4
Matt Guest <mjcguest@gmail.com>
2025-09-23 14:15:42 +0100

    Update gex formula to v1.2.0

--- Formula/gex.rb
+++ Formula/gex.rb
@@ -1,10 +1,11 @@
 class Gex < Formula
   desc "gex hex editor"
   homepage "https://github.com/ki-aura/gex"
-  url "https://github.com/ki-aura/gex/archive/refs/tags/v1.1.0.tar.gz"
-  sha256 "af3b09549aaa0335beac848d459590df44eb2e22d9118f5aaf4d49172b64b387"
+  url "https://github.com/ki-aura/gex/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "5e3033d30839df885de607079d30bdc6c0631a11476251f9e026b1218ed0e5dd"
   license "MIT"
-  depends_on "ncurses" => :build
+  depends_on "ncurses" => :build if OS.mac?
+  depends_on "ncurses" => :build if OS.linux?
   def install
     system "make", "release"
     bin.install "gex"   # replace "gex" with the actual executable name
