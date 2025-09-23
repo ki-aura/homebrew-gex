d647efa8b5faf6c5264b5d8435e951d520b1c320
Matt Guest <mjcguest@gmail.com>
2025-09-23 23:36:23 +0100

    Update gex.rb for v1.3.1

--- Formula/gex.rb
+++ Formula/gex.rb
@@ -4,12 +4,16 @@ class Gex < Formula
   url "https://github.com/ki-aura/gex/archive/refs/tags/v1.3.1.tar.gz"
   sha256 "8b987dd7e008565e457056b883c17f5751e6470ad6dcf10a86599ed950d91b62"
   license "MIT"
+
   depends_on "ncurses"
+
   def install
     system "make", "release"
-    bin.install "gex"   # replace "gex" with the actual executable name
+    bin.install "gex"
   end
+
   test do
-    system "#{bin}/gex"
+    # Check basic execution
+    system "#{bin}/gex", "--version"
   end
 end
