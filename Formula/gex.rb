47550ac008d5ded775ed1975fb026ed8e08975f8
Matt Guest <mjcguest@gmail.com>
2025-09-23 17:02:23 +0100

    Update gex formula to v1.2.0 with portable Makefile

--- Formula/gex.rb
+++ Formula/gex.rb
@@ -2,7 +2,7 @@ class Gex < Formula
   desc "gex hex editor"
   homepage "https://github.com/ki-aura/gex"
   url "https://github.com/ki-aura/gex/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "5e3033d30839df885de607079d30bdc6c0631a11476251f9e026b1218ed0e5dd"
+  sha256 "8a2a0b58e002e59312170c882eb35e577b74bd681502a7685d55e966dc5fc7ec"
   license "MIT"
   depends_on "ncurses" => :build if OS.mac?
   depends_on "ncurses" => :build if OS.linux?
