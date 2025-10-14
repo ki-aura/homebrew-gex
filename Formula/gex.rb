8daa56d8858145fc05e09738f8d9b0503385a2d6
Matt Guest <mjcguest@gmail.com>
2025-10-14 22:33:14 +0100

    ready for v2.2.0

--- Formula/gex.rb
+++ Formula/gex.rb
@@ -1,8 +1,8 @@
 class Gex < Formula
   desc "Tyler Hex Editor"
   homepage "https://github.com/ki-aura/gex"
-  url "https://github.com/ki-aura/gex/archive/refs/tags/v2.1.0.tar.gz"
-  sha256 "bca9b64296126bc291007dd1eae1b485d0dde148cba176edc0efb0c4839b2e18"
+  url "https://github.com/ki-aura/gex/archive/refs/tags/v2.2.0.tar.gz"
+  sha256 "5d725c2a12c52f6d4169a4d4787456b59991939fe22b944115cacf5700545426"
   license "MIT"
 
   depends_on "ncurses"
